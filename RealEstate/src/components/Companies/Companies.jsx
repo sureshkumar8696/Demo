b33662f6b043b5f12e1de917213b5f77bfb0f98b
Suresh Kumar <suresh08696@gmail.com>
2024-02-20 09:48:09 +0530

    Update Some

---
+++ RealEstate/src/components/Companies/Companies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './Companies.css'
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innerWidth flexCenter c-container">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/DLF_logo.svg/2560px-DLF_logo.svg.png" alt="" />
+        <img src="https://cdn.brigadegroup.com/assets/images/logos/brigade-logo_horizontal-2048px.png" alt="" />
+        <img src="https://upload.wikimedia.org/wikipedia/commons/d/dc/Mahindra-Lifespaces.png" alt="" />
+        <img src="https://mma.prnewswire.com/media/1308693/GPL_Logo.jpg?p=facebook" alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default Companies;
